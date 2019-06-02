0224658c26350b36f3857c0cde1bdbfb1cc34b64
Liam <liliwang@cs.stonybrook.edu>
2019-06-01 22:52:32 -0400

    For loop with nested iteration
    In altGrep, trimmed is a variable. The bound variable is introduced and
    used just like a val, only with the val keyword left out.

--- printFilesWithScala.scala
+++ printFilesWithScala.scala
@@ -1,11 +1,12 @@
 val filesHere = (new java.io.File(".")).listFiles
-
+if (args(0) == "default") {
 for (file <- filesHere; if file.getName.endsWith(".scala"))
   println(file)
-
-// to be extra defensive, the following code prints only files and not directories
-for (
-  file <- filesHere
-  if file.isFile
-  if file.getName.endsWith(".scala")
-) println(file)
+} else {
+  // to be extra defensive, the following code prints only files and not directories
+  for (
+    file <- filesHere
+    if file.isFile
+    if file.getName.endsWith(".scala")
+    ) println(file)
+}
