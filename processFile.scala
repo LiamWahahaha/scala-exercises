61fe302e416318bec69ae4387bf6fc1082fc68f2
Liam <liliwang@cs.stonybrook.edu>
2019-06-02 22:09:39 -0400

    Refactor the private function to nested function

--- processFile.scala
+++ processFile.scala
@@ -2,13 +2,13 @@ import scala.io.Source
 
 object LongLines {
   def processFile(filename: String, width: Int) {
+    def processLine(line: String) {
+      if (line.length > width)
+        println(filename + "; " + line.trim)
+    }
     val source = Source.fromFile(filename)
     for (line <- source.getLines)
-      processLine(filename, width, line)
-  }
-  private def processLine(filename: String, width: Int, line: String) {
-    if (line.length > width)
-      println(filename + ": " + line.trim)
+      processLine(line)
   }
   def main(args: Array[String]) {
     val width = args(0).toInt
